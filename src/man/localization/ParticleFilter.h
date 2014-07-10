d59a2d280fae226c193bd16f250fb8b432ea8699
Josh Imhoff <joshimhoff13@gmail.com>
2014-07-09 22:27:25 -0400

    Adjusts walk variances in particle filter

--- src/man/localization/ParticleFilter.h
+++ src/man/localization/ParticleFilter.h
@@ -47,8 +47,10 @@ static const ParticleFilterParams DEFAULT_PARAMS =
     300,                        // Num Particles
     0.2f,                       // Exponential Filter alpha
     0.05f,                      //                    beta
-    0.1f,                    // Variance in x-y odometry
-    0.04f                    // Variance in h odometry
+    // 0.1f,                    // Variance in x-y odometry
+    // 0.04f                    // Variance in h odometry
+    0.005f,                    // Variance in x-y odometry
+    0.0005f                    // Variance in h odometry
 };
 
 /**
